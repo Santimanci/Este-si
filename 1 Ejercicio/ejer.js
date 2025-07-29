0f3b89b9bf4a804de5ddd1471e6c9dad3a4e3277
david santiago mancilla gallo <tu-email@ejemplo.com>
2025-07-28 20:05:22 -0500

    Agregar Carpeta

---
+++ 1 Ejercicio/ejer.js
@@ -0,0 +1,30 @@
+
+function calcular(){
+
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+
+
+
+  let resultado;
+
+  if(document.getElementById("suma").checked){
+    resultado = num1 + num2;
+  }else if(document.getElementById("resta").checked){
+    resultado = num1 - num2;
+  } else if(document.getElementById("multiplicacion").checked){
+    resultado = num1 * num2;
+  } else if(document.getElementById("division").checked){
+    if  (num2 !== 0 ){
+        resultado = num1 / num2;
+    }else {
+        resultado= "Error";
+    }
+} else {
+    resultado = "por favor, seleccione una operación";
+}
+
+
+
+document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
+}
